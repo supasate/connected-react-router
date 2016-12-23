58149459d0ce7c1d91e929de4ca0bd4685bebe6f
Ian Williams <me@ianwillia.ms>
2016-12-23 11:32:46 -0500

    Fixing code style

--- examples/src/reducers/root.js
+++ examples/src/reducers/root.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import counterReducer from './counter'
 
 const rootReducer = combineReducers({
-  count: counterReducer
+  count: counterReducer,
 })
 
 export default rootReducer
