99ef01ad8ec2233fcdaf5221a6f6dfb0f2bf8470
Supasate Choochaisri <supasate.c@gmail.com>
2016-12-25 09:22:17 +0700

    Fix typo

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,6 @@ import { LOCATION_CHANGE } from '../src/actions'
 
 describe('Example Test', () => {
   it('should pass', () => {
-    expect(LOCATION_CHANGE).toEqual('@@router/LOCATION_CHANG')
+    expect(LOCATION_CHANGE).toEqual('@@router/LOCATION_CHANGE')
   })
 })
