6b5172498f1702ce9066dc68efab6d618d2debf8
Supasate Choochaisri <supasate.c@gmail.com>
2016-12-28 13:24:49 +0700

    Test middleware calling history method

---
+++ test/middleware.test.js
@@ -0,0 +1,70 @@
+import configureStore from 'redux-mock-store'
+import routerMiddleware from '../src/middleware'
+import { CALL_HISTORY_METHOD } from '../src/actions'
+
+describe('Middleware', () => {
+  it('calls history method based on action payload values', () => {
+    const history = {
+      location: {},
+      action: 'POP',
+      push: jest.fn(),
+      replace: jest.fn(),
+      go: jest.fn(),
+      goBack: jest.fn(),
+      goForward: jest.fn(),
+    }
+    const middlewares = [routerMiddleware(history)]
+    const mockStore = configureStore(middlewares)
+    const store = mockStore({})
+
+    // push
+    store.dispatch({
+      type: CALL_HISTORY_METHOD,
+      payload: {
+        method: 'push',
+        args: ['/path/to/somewhere'],
+      }
+    })
+    expect(history.push).toBeCalledWith('/path/to/somewhere')
+
+    // replace
+    store.dispatch({
+      type: CALL_HISTORY_METHOD,
+      payload: {
+        method: 'replace',
+        args: ['/path/to/somewhere'],
+      }
+    })
+    expect(history.replace).toBeCalledWith('/path/to/somewhere')
+
+    // go
+    store.dispatch({
+      type: CALL_HISTORY_METHOD,
+      payload: {
+        method: 'go',
+        args: [5],
+      }
+    })
+    expect(history.go).toBeCalledWith(5)
+
+    // goBack
+    store.dispatch({
+      type: CALL_HISTORY_METHOD,
+      payload: {
+        method: 'goBack',
+        args: [],
+      }
+    })
+    expect(history.goBack).toBeCalled()
+
+    // goForward
+    store.dispatch({
+      type: CALL_HISTORY_METHOD,
+      payload: {
+        method: 'goForward',
+        args: [],
+      }
+    })
+    expect(history.goForward).toBeCalled()
+  })
+})
