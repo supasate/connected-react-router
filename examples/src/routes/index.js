a7a2ecc2c70b5685cd523dcfe8f8678f775057ca
Supasate Choochaisri <supasate.c@gmail.com>
2017-01-31 01:33:04 +0700

    Update examples to use react-router v4 beta

--- examples/src/routes/index.js
+++ examples/src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Match, Miss } from 'react-router'
+import { Route, Switch } from 'react-router'
 import Home from '../components/Home'
 import Hello from '../components/Hello'
 import Counter from '../components/Counter'
@@ -9,10 +9,12 @@ import NavBar from '../components/NavBar'
 const routes = (
   <div>
     <NavBar />
-    <Match exactly pattern="/" component={Home} />
-    <Match pattern="/hello" component={Hello} />
-    <Match pattern="/counter" component={Counter} />
-    <Miss component={NoMatch} />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/hello" component={Hello} />
+      <Route path="/counter" component={Counter} />
+      <Route component={NoMatch} />
+    </Switch>
   </div>
 )
 
