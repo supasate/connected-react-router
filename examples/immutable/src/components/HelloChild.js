7521a4efe23a2f61fa83da93411caed61df1085f
Supasate Choochaisri <supasate.c@gmail.com>
2017-02-15 17:54:41 +0700

    Change immutable example to show querystring and hash

--- examples/immutable/src/components/HelloChild.js
+++ examples/immutable/src/components/HelloChild.js
@@ -1,18 +1,36 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-const HelloChild = ({ path }) => (
+const HelloChild = ({ pathname, search, hash }) => (
   <div>
-    Hello-Child at path {path}
+    Hello-Child
+    <ul>
+      <li><Link to="/hello?color=Blue&size=40">with query string</Link></li>
+      <li><Link to="/hello#lovelove">with hash</Link></li>
+    </ul>
+    <div>
+      pathname: {pathname}
+    </div>
+    <div>
+      search: {search}
+    </div>
+    <div>
+      hash: {hash}
+    </div>
   </div>
 )
 
 HelloChild.propTypes = {
-  path: PropTypes.string,
+  pathname: PropTypes.string,
+  search: PropTypes.string,
+  hash: PropTypes.string,
 }
 
 const mapStateToProps = state => ({
-  path: state.getIn(['router', 'location', 'pathname']),
+  pathname: state.getIn(['router', 'location', 'pathname']),
+  search: state.getIn(['router', 'location', 'search']),
+  hash: state.getIn(['router', 'location', 'hash']),
 })
 
 export default connect(mapStateToProps)(HelloChild)
