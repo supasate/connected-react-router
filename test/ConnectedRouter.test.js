60e010b09bccaa8e1b25e866eba822f3dd16c9a2
John Mitchell <john.mitchell@ignitionone.com>
2017-04-12 14:20:30 -0400

    upgrade to react 15.5 and react-router 4.1

--- test/ConnectedRouter.test.js
+++ test/ConnectedRouter.test.js
@@ -1,4 +1,5 @@
-import React, { Children, Component, PropTypes } from 'react'
+import React, { Children, Component } from 'react'
+import PropTypes from 'prop-types'
 import configureStore from 'redux-mock-store'
 import { createStore, combineReducers } from 'redux'
 import { ActionCreators, instrument } from 'redux-devtools'
