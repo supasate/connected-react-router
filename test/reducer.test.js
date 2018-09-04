ce53cc05457edb84974e4be9f49c4229b93e7046
Joey Figaro <joey@joeyfigaro.com>
2018-09-04 15:39:41 -0400

    implemented feedback/made requested formatting changes

--- test/reducer.test.js
+++ test/reducer.test.js
@@ -7,166 +7,166 @@ import { connectRouter as connectRouterImmutable } from '../src/immutable'
 import { connectRouter as connectRouterSeamlessImmutable } from '../src/seamless-immutable'
 
 describe('connectRouter', () => {
-	let mockHistory
+  let mockHistory
 
-	beforeEach(() => {
-		mockHistory = {
-			location: {
-				pathname: '/',
-				search: '',
-				hash: '',
-			},
-			action: 'POP',
-		}
-	})
+  beforeEach(() => {
+    mockHistory = {
+      location: {
+        pathname: '/',
+        search: '',
+        hash: '',
+      },
+      action: 'POP',
+    }
+  })
 
-	describe('with plain structure', () => {
-		it('creates new root reducer with router reducer inside', () => {
-			const mockReducer = (state = {}, action) => {
-				switch (action.type) {
-					default:
-						return state
-				}
-			}
-			const rootReducer = combineReducers({
-				mock: mockReducer,
-			})
+  describe('with plain structure', () => {
+    it('creates new root reducer with router reducer inside', () => {
+      const mockReducer = (state = {}, action) => {
+        switch (action.type) {
+          default:
+            return state
+        }
+      }
+      const rootReducer = combineReducers({
+        mock: mockReducer,
+      })
 
-			const rootReducerWithRouter = connectRouter(mockHistory)(rootReducer)
-			const currentState = {
-				router: {
-					location: {
-						pathname: '/',
-						search: '',
-						hash: '',
-					},
-					action: 'POP',
-				},
-			}
-			const action = {
-				type: LOCATION_CHANGE,
-				payload: {
-					location: {
-						pathname: '/path/to/somewhere',
-						search: '?query=test',
-						hash: '',
-					},
-					action: 'PUSH',
-				}
-			}
-			const nextState = rootReducerWithRouter(currentState, action)
-			const expectedState = {
-				mock: {},
-				router: {
-					location: {
-						pathname: '/path/to/somewhere',
-						search: '?query=test',
-						hash: '',
-					},
-					action: 'PUSH',
-				},
-			}
-			expect(nextState).toEqual(expectedState)
-		})
-	})
+      const rootReducerWithRouter = connectRouter(mockHistory)(rootReducer)
+      const currentState = {
+        router: {
+          location: {
+            pathname: '/',
+            search: '',
+            hash: '',
+          },
+          action: 'POP',
+        },
+      }
+      const action = {
+        type: LOCATION_CHANGE,
+        payload: {
+          location: {
+            pathname: '/path/to/somewhere',
+            search: '?query=test',
+            hash: '',
+          },
+          action: 'PUSH',
+        }
+      }
+      const nextState = rootReducerWithRouter(currentState, action)
+      const expectedState = {
+        mock: {},
+        router: {
+          location: {
+            pathname: '/path/to/somewhere',
+            search: '?query=test',
+            hash: '',
+          },
+          action: 'PUSH',
+        },
+      }
+      expect(nextState).toEqual(expectedState)
+    })
+  })
 
-	describe('with immutable structure', () => {
-		it('creates new root reducer with router reducer inside', () => {
-			const mockReducer = (state = Immutable.Map(), action) => {
-				switch (action.type) {
-					default:
-						return state
-				}
-			}
-			const rootReducer = combineReducersImmutable({
-				mock: mockReducer,
-			})
+  describe('with immutable structure', () => {
+    it('creates new root reducer with router reducer inside', () => {
+      const mockReducer = (state = Immutable.Map(), action) => {
+        switch (action.type) {
+          default:
+            return state
+        }
+      }
+      const rootReducer = combineReducersImmutable({
+        mock: mockReducer,
+      })
 
-			const rootReducerWithRouter = connectRouterImmutable(mockHistory)(rootReducer)
-			const currentState = Immutable.fromJS({
-				router: {
-					location: {
-						pathname: '/',
-						search: '',
-						hash: '',
-					},
-					action: 'POP',
-				},
-			})
-			const action = {
-				type: LOCATION_CHANGE,
-				payload: {
-					location: {
-						pathname: '/path/to/somewhere',
-						search: '?query=test',
-						hash: '',
-					},
-					action: 'PUSH',
-				}
-			}
-			const nextState = rootReducerWithRouter(currentState, action)
-			const expectedState = Immutable.fromJS({
-				mock: {},
-				router: {
-					location: {
-						pathname: '/path/to/somewhere',
-						search: '?query=test',
-						hash: '',
-					},
-					action: 'PUSH',
-				},
-			})
-			expect(nextState).toEqual(expectedState)
-		})
-	})
+      const rootReducerWithRouter = connectRouterImmutable(mockHistory)(rootReducer)
+      const currentState = Immutable.fromJS({
+        router: {
+          location: {
+            pathname: '/',
+            search: '',
+            hash: '',
+          },
+          action: 'POP',
+        },
+      })
+      const action = {
+        type: LOCATION_CHANGE,
+        payload: {
+          location: {
+            pathname: '/path/to/somewhere',
+            search: '?query=test',
+            hash: '',
+          },
+          action: 'PUSH',
+        }
+      }
+      const nextState = rootReducerWithRouter(currentState, action)
+      const expectedState = Immutable.fromJS({
+        mock: {},
+        router: {
+          location: {
+            pathname: '/path/to/somewhere',
+            search: '?query=test',
+            hash: '',
+          },
+          action: 'PUSH',
+        },
+      })
+      expect(nextState).toEqual(expectedState)
+    })
+  })
 
-	describe('with seamless immutable structure', () => {
-		it('creates new root reducer with router reducer inside', () => {
-			const mockReducer = (state = {}, action) => {
-				switch (action.type) {
-					default:
-						return state
-				}
-			}
-			const rootReducer = combineReducersSeamlessImmutable({
-				mock: mockReducer,
-			})
+  describe('with seamless immutable structure', () => {
+    it('creates new root reducer with router reducer inside', () => {
+      const mockReducer = (state = {}, action) => {
+        switch (action.type) {
+          default:
+            return state
+        }
+      }
+      const rootReducer = combineReducersSeamlessImmutable({
+        mock: mockReducer,
+      })
 
-			const rootReducerWithRouter = connectRouterSeamlessImmutable(mockHistory)(rootReducer)
-			const currentState = {
-				router: {
-					location: {
-						pathname: '/',
-						search: '',
-						hash: '',
-					},
-					action: 'POP',
-				},
-			}
-			const action = {
-				type: LOCATION_CHANGE,
-				payload: {
-					location: {
-						pathname: '/path/to/somewhere',
-						search: '?query=test',
-						hash: '',
-					},
-					action: 'PUSH',
-				}
-			}
-			const nextState = rootReducerWithRouter(currentState, action)
-			const expectedState = {
-				mock: {},
-				router: {
-					location: {
-						pathname: '/path/to/somewhere',
-						search: '?query=test',
-						hash: '',
-					},
-					action: 'PUSH',
-				},
-			}
-			expect(nextState).toEqual(expectedState)
-		})
-	})
+      const rootReducerWithRouter = connectRouterSeamlessImmutable(mockHistory)(rootReducer)
+      const currentState = {
+        router: {
+          location: {
+            pathname: '/',
+            search: '',
+            hash: '',
+          },
+          action: 'POP',
+        },
+      }
+      const action = {
+        type: LOCATION_CHANGE,
+        payload: {
+          location: {
+            pathname: '/path/to/somewhere',
+            search: '?query=test',
+            hash: '',
+          },
+          action: 'PUSH',
+        }
+      }
+      const nextState = rootReducerWithRouter(currentState, action)
+      const expectedState = {
+        mock: {},
+        router: {
+          location: {
+            pathname: '/path/to/somewhere',
+            search: '?query=test',
+            hash: '',
+          },
+          action: 'PUSH',
+        },
+      }
+      expect(nextState).toEqual(expectedState)
+    })
+  })
 })
