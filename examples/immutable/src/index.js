5a7f5839997ef8a4a56561325ec926857a740268
Rhuan Santos <rhuansantosdev@gmail.com>
2018-11-23 22:14:39 -0500

    remove not used variable

--- examples/immutable/src/index.js
+++ examples/immutable/src/index.js
@@ -1,27 +1,23 @@
-import { AppContainer } from 'react-hot-loader'
-import { applyMiddleware, compose, createStore } from 'redux'
-import { createBrowserHistory } from 'history'
-import { routerMiddleware, connectRouter } from 'connected-react-router/immutable'
-import { Provider } from 'react-redux'
-import Immutable from 'immutable'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import rootReducer from './reducers'
+import { AppContainer } from "react-hot-loader";
+import { applyMiddleware, compose, createStore } from "redux";
+import { createBrowserHistory } from "history";
+import { routerMiddleware } from "connected-react-router/immutable";
+import { Provider } from "react-redux";
+import Immutable from "immutable";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import rootReducer from "./reducers";
 
-const history = createBrowserHistory()
+const history = createBrowserHistory();
 
-const initialState = Immutable.Map()
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const initialState = Immutable.Map();
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer(history),
   initialState,
-  composeEnhancer(
-    applyMiddleware(
-      routerMiddleware(history),
-    ),
-  ),
-)
+  composeEnhancer(applyMiddleware(routerMiddleware(history)))
+);
 
 const render = () => {
   ReactDOM.render(
@@ -30,21 +26,21 @@ const render = () => {
         <App history={history} />
       </Provider>
     </AppContainer>,
-    document.getElementById('react-root')
-  )
-}
+    document.getElementById("react-root")
+  );
+};
 
-render()
+render();
 
 // Hot reloading
 if (module.hot) {
   // Reload components
-  module.hot.accept('./App', () => {
-    render()
-  })
+  module.hot.accept("./App", () => {
+    render();
+  });
 
   // Reload reducers
-  module.hot.accept('./reducers', () => {
-    store.replaceReducer(rootReducer(history))
-  })
+  module.hot.accept("./reducers", () => {
+    store.replaceReducer(rootReducer(history));
+  });
 }
