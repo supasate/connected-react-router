8409dbc115e2d72c4d1560c1eaf92dd52984ac57
Rhuan Santos <rhuansantosdev@gmail.com>
2018-11-23 23:23:29 -0500

    remove linter autofixes

--- examples/immutable/src/index.js
+++ examples/immutable/src/index.js
@@ -16,7 +16,12 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
   rootReducer(history),
   initialState,
-  composeEnhancer(applyMiddleware(routerMiddleware(history)))
+  composeEnhancer(
+    applyMiddleware(
+      routerMiddleware(history
+      )
+    )
+  )
 )
 
 const render = () => {
