d2ad693a34fc00855f9dc1dcdb501c4e33bcd18a
Daniel Perez Alvarez <unindented@gmail.com>
2018-12-07 14:36:43 -0800

    Output ESM build, and make module tree-shakeable

--- src/immutable.js
+++ src/immutable.js
@@ -1,19 +1,11 @@
-import createAll from './createAll'
+import createConnectedRouter from "./ConnectedRouter"
+import createConnectRouter from "./reducer"
+import createSelectors from "./selectors"
 import immutableStructure from './structure/immutable'
 
-export const {
-  LOCATION_CHANGE,
-  CALL_HISTORY_METHOD,
-  push,
-  replace,
-  go,
-  goBack,
-  goForward,
-  routerActions,
-  ConnectedRouter,
-  connectRouter,
-  routerMiddleware,
-  getLocation,
-  getAction,
-  createMatchSelector,
-} = createAll(immutableStructure)
+export { LOCATION_CHANGE, CALL_HISTORY_METHOD, push, replace, go, goBack, goForward, routerActions } from "./actions"
+export { default as routerMiddleware } from "./middleware"
+
+export const ConnectedRouter = /*#__PURE__*/ createConnectedRouter(immutableStructure)
+export const connectRouter = /*#__PURE__*/ createConnectRouter(immutableStructure)
+export const { getLocation, getAction, createMatchSelector } = /*#__PURE__*/ createSelectors(immutableStructure)
