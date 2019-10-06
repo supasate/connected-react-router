e91df3948ff5257282285a50ed3680a61e541e0a
John C. Kennedy <devjckennedy@gmail.com>
2019-10-06 18:05:25 +0900

    fix code style

--- src/reducer.js
+++ src/reducer.js
@@ -5,26 +5,37 @@ import { LOCATION_CHANGE } from './actions'
  * Utilises the search prop of location to construct query.
  */
 const injectQuery = (location) => {
-  if (!location) return location
+  if (!location) {
+    return location
+  }
+
   const searchQuery = location.search || window.location.search
 
   if (typeof searchQuery !== 'string' || searchQuery.length === 0) {
-    return { ...location, query: {} }
+    return {
+      ...location,
+      query: {}
+    }
   }
 
   // Ignore the `?` part of the search string e.g. ?username=codejockie
   const search = searchQuery.substring(1)
-
   // Split the query string on `&` e.g. ?username=codejockie&name=Kennedy
   const queries = search.split('&')
   // Contruct query
   const query = queries.reduce((acc, currentQuery) => {
     // Split on `=`, to get key and value
     const [queryKey, queryValue] = currentQuery.split('=')
-    return { ...acc, [queryKey]: queryValue }
+    return {
+      ...acc,
+      [queryKey]: queryValue
+    }
   }, {})
 
-  return { ...location, query }
+  return {
+    ...location,
+    query
+  }
 }
 
 const createConnectRouter = (structure) => {
