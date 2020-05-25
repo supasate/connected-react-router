25c4f801109f13d42291e8bbdcf85648480a4c86
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-05-25 21:21:04 +0700

    Update index.js

--- src/structure/immutable/index.js
+++ src/structure/immutable/index.js
@@ -1,11 +1,11 @@
-import { Iterable, fromJS } from 'immutable'
+import { Iterable, fromJS, merge } from 'immutable'
 import getIn from './getIn'
 
 const structure = {
   fromJS: jsValue => fromJS(jsValue, (key, value) =>
     Iterable.isIndexed(value) ? value.toList() : value.toMap()),
   getIn,
-  merge: (state, payload) => state.merge(payload),
+  merge: (state, payload) => merge(state, payload),
   toJS: value => Iterable.isIterable(value) ? value.toJS() : value,
 }
 
