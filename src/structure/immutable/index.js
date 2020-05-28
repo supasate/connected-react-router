e8f2363a72f2f4959a96f1b5b53e06ab3027640c
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-05-28 17:14:18 +0700

    fix state.merge error

--- src/structure/immutable/index.js
+++ src/structure/immutable/index.js
@@ -1,12 +1,12 @@
-import { Iterable, fromJS } from 'immutable'
+import Immutable from 'immutable'
 import getIn from './getIn'
 
 const structure = {
-  fromJS: jsValue => fromJS(jsValue, (key, value) =>
-    Iterable.isIndexed(value) ? value.toList() : value.toMap()),
+  fromJS: jsValue => Immutable.fromJS(jsValue, (key, value) =>
+    Immutable.Iterable.isIndexed(value) ? value.toList() : value.toMap()),
   getIn,
-  merge: (state, payload) => state.merge(payload),
-  toJS: value => Iterable.isIterable(value) ? value.toJS() : value,
+  merge: (state, payload) => Immutable.merge(state, payload),
+  toJS: value => Immutable.Iterable.isIterable(value) ? value.toJS() : value,
 }
 
 export default structure
